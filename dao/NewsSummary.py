8e9110920fddc6e102ba8270f03715027e751d7b
yaocong <rogwel@vip.qq.com>
2021-05-23 14:54:06 +0800

    add: kw_virtual column as a generated column in database.

--- dao/NewsSummary.py
+++ dao/NewsSummary.py
@@ -8,6 +8,7 @@ class NewsSummary(Base):
     summary = Column(String(3000))
     kw = Column(JSON)
     publish_time = Column(DateTime)
+    kw_virtual = Column(String(255),primary_key=True)
 
     def __init__(self,article_id,title,summary,kw,publish_time):
         self.article_id = article_id
